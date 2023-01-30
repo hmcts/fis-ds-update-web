6913d839146ce415757c7795bad5e68f341a454f
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2023-01-30 16:13:44 +0000

    added test cases for getController

---
+++ src/main/steps/upload-documents/getController.test.ts
@@ -0,0 +1,28 @@
+
+import { mockRequest } from '../../../test/unit/mocks/mockRequest';
+import { mockResponse } from '../../../test/unit/mocks/mockResponse';
+import { FieldPrefix } from '../../app/case/case';
+import { UPLOAD_DOCUMENT } from '../../steps/urls';
+
+import DocumentUpload from './getController';
+
+describe('Test URL endpoints', () => {
+  const controller = new DocumentUpload('page', () => ({}), FieldPrefix.APPLICANT);
+  const res = mockResponse();
+  const req = mockRequest();
+  test('should be able to render the page and not show error page', async () => {
+    await controller.get(req, res);
+    expect(res.render).not.toHaveBeenCalledWith('error');
+  });
+
+  test('should be able to remove the documents from session, async', async () => {
+    const parsedRequest = {
+      ...req,
+      session: {
+        caseDocuments: [{ documentId: 'abc' }, { documentId: 'ubc' }],
+      },
+    };
+    await controller.removeExistingConsentDocument('abc', parsedRequest, res);
+    expect(res.redirect).not.toHaveBeenCalledWith(UPLOAD_DOCUMENT);
+  });
+});
