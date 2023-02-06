d3335f2570a5de459f5de5319c76fadca5da504f
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2023-02-06 11:15:53 +0000

    fixed test cases for sequences of upload document

--- src/main/steps/upload-documents/uploadDocumentsSequence.test.ts
+++ src/main/steps/upload-documents/uploadDocumentsSequence.test.ts
@@ -2,8 +2,10 @@ import { uploadDocumentsSequence } from './uploadDocumentsSequence';
 
 describe('Testing the sequence for uploading documents', () => {
   test('upload document sequence', () => {
+    expect(Array.isArray(uploadDocumentsSequence)).toBe(true);
     expect(uploadDocumentsSequence[0].url).toBe('/upload-documents');
     expect(uploadDocumentsSequence[0].showInSection).toBe('upload-documents');
+    expect(uploadDocumentsSequence[0].getNextStep({})).toBe('/upload-documents');
     expect(uploadDocumentsSequence[0].getNextStep).not.toBe(() => '');
   });
 });
