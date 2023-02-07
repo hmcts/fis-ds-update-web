35bd087b63751b79564f794c4971a1977f3f6223
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2023-02-07 15:41:06 +0000

    added test case for upload documents

--- src/main/steps/upload-documents/postController.test.ts
+++ src/main/steps/upload-documents/postController.test.ts
@@ -1,13 +1,48 @@
-import { FormContent } from '../../app/form/Form';
+import axios from 'axios';
+
+import { FormContent } from '../../../main/app/form/Form';
+import { mockRequest } from '../../../test/unit/utils/mockRequest';
+import { mockResponse } from '../../../test/unit/utils/mockResponse';
 
 import UploadDocumentController from './postController';
+jest.mock('axios');
+let req, res;
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+beforeEach(() => {
+  req = mockRequest();
+  res = mockResponse();
+});
 
 const mockFormContent = {
   fields: {},
 } as unknown as FormContent;
+
 const controller = new UploadDocumentController(mockFormContent.fields);
+
 describe('Testing the post controller', () => {
-  test('upload document sequence', () => {
-    expect(controller.post).not.toBe(1);
+  test('upload document sequence', async () => {
+    req = mockRequest({
+      body: {
+        saveAndContinue: true,
+        files: { documents: {} },
+      },
+      session: {
+        applicantCaseId: 'caseRefId',
+        caseDocuments: [],
+      },
+    });
+    const data = {
+      status: 'Success',
+      document: {
+        url: 'http://demo.com',
+        fileName: 'Screenshot 2023-01-24 at 11.52.19.png',
+        documentId: '93de8780-e3f3',
+        binaryUrl: 'http://demon.com',
+      },
+    };
+    mockedAxios.post.mockResolvedValue({ data });
+    await controller.post(req, res);
+    expect(res.redirect).toHaveBeenCalled();
   });
 });
