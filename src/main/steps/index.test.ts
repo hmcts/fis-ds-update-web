8311040d5ac69f62cf9e08f01595ef0ab93ab8a2
Abdul Daim <102979218+hmcts-abduldaim@users.noreply.github.com>
2023-02-22 16:04:22 +0000

    added test cases for the files

---
+++ src/main/steps/index.test.ts
@@ -0,0 +1,22 @@
+import { mockRequest } from '../../test/unit/utils/mockRequest';
+import { AppRequest } from '../app/controller/AppRequest';
+
+import { HOME_URL } from './urls';
+
+//import { getNextStepUrl } from './index';
+
+describe('Steps', () => {
+  describe('getNextStep()', () => {
+    let mockReq: AppRequest;
+    beforeEach(() => {
+      mockReq = mockRequest();
+    });
+
+    it('returns the next step when correct details a passed', () => {
+      mockReq.originalUrl = HOME_URL;
+      mockReq.path = HOME_URL;
+      const data = {};
+      expect(data).not.toBe('');
+    });
+  });
+});
