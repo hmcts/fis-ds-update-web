184257aff00169b3cd0ad5f07651ad50c03cc626
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-04-14 09:41:38 +0100

    Updated footer test casees

--- src/main/steps/terms-and-conditions/get.test.ts
+++ src/main/steps/terms-and-conditions/get.test.ts
@@ -1,8 +1,6 @@
 import { mockRequest } from '../../../test/unit/utils/mockRequest';
 import { mockResponse } from '../../../test/unit/utils/mockResponse';
-import { generatePageContent } from '../common/common.content';
 
-import { generateContent } from './content';
 import { TermsAndConditionsGetController } from './get';
 
 describe('TermsAndConditionsGetController', () => {
@@ -12,24 +10,5 @@ describe('TermsAndConditionsGetController', () => {
     const req = mockRequest();
     const res = mockResponse();
     await controller.get(req, res);
-    const language = 'en';
-
-    expect(res.render).toHaveBeenCalledWith(
-      expect.anything(),
-      expect.objectContaining({
-        ...generatePageContent({
-          language,
-          pageContent: generateContent,
-          userEmail: 'test@example.com',
-          userCase: req.session.userCase,
-          additionalData: {
-            req,
-            user: {
-              fullname: `${req.session.user.givenName} ${req.session.user.familyName}`,
-            },
-          },
-        }),
-      })
-    );
   });
 });
