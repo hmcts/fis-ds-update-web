1fe74ab75301bbafe47bcfeeaab9f5c7e6d9bcf1
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-09-29 22:52:43 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,9 +30,3 @@ module "fis-ds-update-redis6" {
   public_network_access_enabled = false
   common_tags  = var.common_tags
 }
-
-resource "azurerm_key_vault_secret" "redis6_access_key" {
-  name         = "redis6-access-key"
-  value        = module.fis-kv-redis6.access_key
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
