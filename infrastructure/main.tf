cd4c65587017de1a4a95117101fe0f2bf3da26a1
yogendraU <66823759+yogendraU@users.noreply.github.com>
2023-09-29 23:08:22 +0100

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,30 +3,23 @@ provider "azurerm" {
 }
 
 locals {
-  vaultName = "${var.product}-${var.env}"
+  vaultName = "${var.product}-kv-${var.env}"
 }
-
-data "azurerm_subnet" "core_infra_redis_subnet" {
-  name                 = "core-infra-subnet-1-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-  resource_group_name = "core-infra-${var.env}"
-}
-
 
 data "azurerm_key_vault" "key_vault" {
   name = local.vaultName
   resource_group_name = local.vaultName
 }
 
-module "fis-ds-update-redis6" {
+module "fis-ds-update-session-storage" {
   source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product  = var.product
-  name     = "${var.product}-${var.component}-${var.env}"
+  product  = "${var.product}-${var.component}-${var.env}"
   location = var.location
   env      = var.env
+  common_tags  = var.common_tags
   private_endpoint_enabled = true
   redis_version = "6"
   business_area = "cft"
   public_network_access_enabled = false
-  common_tags  = var.common_tags
 }
+
