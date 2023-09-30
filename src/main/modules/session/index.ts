53278ae4364f4b209e29668130cdad80c6721cca
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-10-01 00:12:34 +0100

    logg

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -50,7 +50,7 @@ export class SessionStorage {
       console.log('inside redis client ');
       console.log(client);
       console.log('after client console');
-      console.log('config.get("session.redis.key")')
+      console.log('config.get("session.redis.key")');
       console.log(config.get('session.redis.key'));
       return new RedisStore({ client });
     }
