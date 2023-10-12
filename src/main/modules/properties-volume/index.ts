165ef6928ae774a3e3fa17ee9aa1f031f6aafee3
Yogendra Upasani <yogendra.upasani@hmcts.net>
2023-10-12 18:53:41 +0100

    pointing to Redis v6 key

--- src/main/modules/properties-volume/index.ts
+++ src/main/modules/properties-volume/index.ts
@@ -9,8 +9,8 @@ export class PropertiesVolume {
       propertiesVolume.addTo(config);
 
       this.setSecret('secrets.fis-kv.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-      this.setSecret('secrets.fis-kv.redis-access-key', 'session.redis.key');
-      this.setSecret('secrets.fis-kv.redis-access-key', 'session.secret');
+      this.setSecret('secrets.fis-kv.redis-access-key-v6', 'session.redis.key');
+      this.setSecret('secrets.fis-kv.redis-access-key-v6', 'session.secret');
     }
   }
 
