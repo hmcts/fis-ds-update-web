79c04fd0b52b44117f198908eb0a3760e67e98a6
gabriellemagruder <gabrielle.magruder@solirius.com>
2024-01-25 15:06:14 +0000

    Make content changes to landing page

--- src/main/steps/dss-update/start/content.ts
+++ src/main/steps/dss-update/start/content.ts
@@ -5,18 +5,20 @@ import { FormContent } from '../../../app/form/Form';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const en = () => ({
   serviceName: 'DSS Update Case',
-  title: 'Update a case',
-  paragraph1: 'Use this service to update a case that has already been submitted.',
-  paragraph2: 'You can upload additional evidences and documents.',
-  paragraph3: 'You will need the case reference number to start.',
+  title: 'Update an existing First-tier Tribunal case',
+  paragraph1: 'Use this service to update a case that has already been submitted to the First-tier Tribunal.',
+  paragraph2:
+    'You may want to send the Tribunal some extra paperwork that was not available at the time you initially submitted your tribunal form. Or you may want to give the Tribunal some extra details about aspects of your case.',
+  paragraph3: 'To update an existing case, you will need the case reference number.',
 });
 
 export const cy = () => ({
   serviceName: 'DSS Update Case',
-  title: 'Update a case - welsh',
-  paragraph1: 'Use this service to update a case that has already been submitted. - welsh',
-  paragraph2: 'You can upload additional evidences and documents. - welsh',
-  paragraph3: 'You will need the case reference number to start. - welsh',
+  title: 'Update an existing First-tier Tribunal case - welsh',
+  paragraph1: 'Use this service to update a case that has already been submitted to the First-tier Tribunal. - welsh',
+  paragraph2:
+    'You may want to send the Tribunal some extra paperwork that was not available at the time you initially submitted your tribunal form. Or you may want to give the Tribunal some extra details about aspects of your case. - welsh',
+  paragraph3: 'To update an existing case, you will need the case reference number. - welsh',
 });
 
 const languages = {
