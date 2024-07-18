58709df4d2fd78dc8c26613584f5f68ee8e00fce
Yogendra Upasani <yogendra.upasani@hmcts.net>
2024-07-18 18:41:12 +0100

    updated redis packages to fix frequent restart issue

--- src/main/modules/session/index.ts
+++ src/main/modules/session/index.ts
@@ -1,29 +1,31 @@
 import config from 'config';
-import RedisStore from 'connect-redis';
+import ConnectRedis from 'connect-redis';
 import cookieParser from 'cookie-parser';
 import { Application } from 'express';
 import session from 'express-session';
 import * as redis from 'redis';
 import FileStoreFactory from 'session-file-store';
 
+const RedisStore = ConnectRedis(session);
 const FileStore = FileStoreFactory(session);
+
+export const cookieMaxAge = 21 * (60 * 1000); // 21 minutes
 
 export class SessionStorage {
   public enableFor(app: Application): void {
     app.use(cookieParser());
-    app.set('trust proxy', 1);
 
     app.use(
       session({
-        name: 'fis-ds-update-web-session',
+        name: 'dss-update-case-session',
         resave: false,
         saveUninitialized: false,
         secret: config.get('session.secret'),
         cookie: {
           httpOnly: true,
-          maxAge: config.get('session.maxAge'),
+          ...(config.get('session.secureCookie') === 'true' ? { secure: true } : {}),
+          maxAge: cookieMaxAge,
           sameSite: 'lax', // required for the oauth2 redirect
-          secure: true,
         },
         rolling: true, // Renew the cookie for another 20 minutes on each request
         store: this.getStore(app),
@@ -35,23 +37,14 @@ export class SessionStorage {
     const redisHost = config.get('session.redis.host');
     if (redisHost) {
       const client = redis.createClient({
-        socket: {
-          host: redisHost as string,
-          port: 6380,
-          connectTimeout: 15000,
-          tls: true,
-        },
+        host: redisHost as string,
         password: config.get('session.redis.key') as string,
+        port: 6380,
+        tls: true,
+        connect_timeout: 15000,
       });
 
-      client.connect();
-
       app.locals.redisClient = client;
-      console.log('inside redis client ');
-      console.log(client);
-      console.log('after client console');
-      console.log('config.get("session.redis.key")');
-      console.log(config.get('session.redis.key'));
       return new RedisStore({ client });
     }
 
